89a67e4a502c24bdd10bea5f4cbe66c6aa775bb1
z31a <fsck3d@protonmail.com>
2023-12-13 23:32:42 +0530

    parttwo -- failure

--- day8/main.go
+++ day8/main.go
@@ -11,18 +11,78 @@ const (
     node_S, node_F string = "AAA", "ZZZ"
 )
 
-var status_F bool
+/* to store instruction set */
 var instruction_S string
+/* node map container node = (L_node, R_node) */
 var nodes_N = make(map[string]([]string))
 
+/* function to start with nodes that ends with A like __A 
+   and iterate it simultaneously to reach end nodes with __Z
+   not working out with this implementation
+   concurrent might be the right thing to do */
 func travel_AN() {
+    status_F := false
+    steps := 0
+    nodes := ends_A()
+    count_Z := len(nodes)
+
+    /* iterating till Z nodes are reached */
+    for !status_F {
+        for dir := 0; dir < len(instruction_S); dir++ {
+            z := 0
+            temp_N := []string{}
+            /* starting with nodes that ends with A */
+            for _, ele_N := range nodes {
+                if instruction_S[dir] == byte('L') {
+                    temp_N = append(temp_N, nodes_N[ele_N][0])
+                } else {
+                    temp_N = append(temp_N, nodes_N[ele_N][1])
+                }
+            }
+            /* reaching next stop as per instruction to 
+               increment the steps value */
+            steps++
+
+            /* checking if we reached the destination 
+               that is nodes ending with Z */
+            for _, ele_N := range temp_N {
+                if ele_N[2] == byte('Z') {
+                    z++
+                }
+            }
+
+            if z == count_Z {
+                status_F = true
+                break
+            } else {
+                nodes = temp_N
+            }
+        }
+
+        /* iterating over instruction set if destination is not reached */
+        if status_F != true {
+            continue
+        }
+    }
+}
+
+/* function to find nodes that ends with __A */
+func ends_A() []string {
+    nodes_A := []string{}
+    for key_N := range nodes_N {
+        if key_N[2] == byte('A') {
+            nodes_A = append(nodes_A, key_N)
+        }
+    }
+    return nodes_A
 }
 
 /* function to iterate given direction instruction
    starting from AAA node to reach ZZZ */
 func travel_N() {
-    /* initiating steps counter and node */
+    /* initiating steps counter, status_F and node */
     steps := 0
+    status_F := false
     node := node_S
 
     /* iterate over to reach ZZZ node */
@@ -108,6 +168,6 @@ func main() {
     travel_N()
 
     /* part two : travel from all nodes that ends with A */
-    travel_AN()
+    //travel_AN()
 }
 
