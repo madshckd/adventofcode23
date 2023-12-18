c41f023e189f3ab64ca65af646c5071f35838282
z31a <fsck3d@protonmail.com>
2023-12-19 00:39:52 +0530

    dumb-struck

--- day15/main.go
+++ day15/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+    "strconv"
 	"os"
 )
 
+type box [](map[string]int)
+
 /* global : hashresult of string & sum of hashresult */
-var sum, hashResult int
+var sum, hashResult, focusResult int
+var boxList [256]box
 
 /* hashing algorithm */
 /* 
@@ -21,6 +25,49 @@ func findHash(input byte) {
     hashResult *= 17
     hashResult %= 256
 }
+
+func toInt(numstr string) int {
+    num, err := strconv.Atoi(numstr)
+
+    if err != nil {
+        fmt.Println("String to Int conversion error : ", err)
+    }
+    return num
+}
+
+func boxDetails(boxInput int, boxLabel string, lenseOperation, lenseFocal byte) {
+    if lenseOperation == byte('-') {
+        for _, labels := range boxList[boxInput] {
+            if _, isLabelExists := labels[boxLabel[0:2]]; isLabelExists {
+                delete(labels, boxLabel[0:2])
+            }
+        }
+    } else {
+        for _, labels := range boxList[boxInput] {
+            if _, isLabelExists := labels[boxLabel[0:2]]; isLabelExists {
+                labels[boxLabel[0:2]] = toInt(string(lenseFocal))
+                return
+            }
+        }
+        boxList[boxInput] = append(boxList[boxInput], map[string]int{boxLabel[0:2] : toInt(string(lenseFocal))}) 
+    }
+}
+
+func totalFocus() {
+    for list := range boxList {
+        pos := 0
+        for _, ele := range boxList[list] {
+            if len(ele) > 0{
+                fmt.Println(ele)
+                for _, focus := range ele {
+                    focusResult += ((pos+1) * focus * (list+1))
+                    pos++
+                }
+            }
+        }
+    }
+}
+
 
 /* __main__ */
 func main() {
@@ -39,22 +86,59 @@ func main() {
     /* buffer to read each character from input file */
     buf := bufio.NewReader(file)
 
+    lenseNext := false
+    lenseOperation := byte('-')
+    boxInput := 0
+    boxLabel := ""
     for {
-        if byteInput, err := buf.ReadByte(); err != nil {
+        byteInput, err := buf.ReadByte() 
+
+        if lenseNext {
+            if byteInput == byte(',') {
+                boxDetails(boxInput, boxLabel, lenseOperation, 0)
+            } else {
+                boxDetails(boxInput, boxLabel, lenseOperation, byteInput)
+            }
+            lenseNext = false
+        }
+
+        if err != nil {
                 break
         } else {
             /* discarding comma and newline characters */
             if byteInput == byte(',') || byteInput == byte('\n') {
                 sum+= hashResult
+                boxLabel = ""
                 hashResult = 0
                 continue
             } else {
+                if byteInput == byte('-') {
+                    boxInput = hashResult
+                    lenseOperation = byte('-')
+                    lenseNext = true
+                    
+                } else if byteInput == byte('=') {
+                    boxInput = hashResult
+                    lenseOperation = byte('=')
+                    lenseNext = true
+                } 
                 findHash(byteInput)
+                boxLabel += string(byteInput)
             }
         }
     }
 
+    totalFocus()
+
     /* part one : result */
     fmt.Println("Part one (sum of hashes) : ", sum)
+
+    /* part two : result */
+    fmt.Println("Part two (sum of focal Lengths) : ", focusResult)
+
+    for _, ele := range boxList {
+        fmt.Println(ele)
+    }
+
 
 }
