993b4d3e7116bb4ce7901ab3c0c965f470308901
z31a <fsck3d@protonmail.com>
2023-12-19 08:02:32 +0530

    day15 -- completed

--- day15/main.go
+++ day15/main.go
@@ -7,11 +7,18 @@ import (
 	"os"
 )
 
+const (
+    BOX_N = 256         /* total boxes */
+    MULTIPLY_H = 17     /* multiplied by : used in hash function */
+    DIVIDE_H = 256      /* divided by : used in hash function */
+)
+
+/* box type a slice of maps { string : int } */
 type box [](map[string]int)
 
 /* global : hashresult of string & sum of hashresult */
 var sum, hashResult, focusResult int
-var boxList [256]box
+var boxList [BOX_N]box
 
 /* hashing algorithm */
 /* 
@@ -22,10 +29,11 @@ var boxList [256]box
 */
 func findHash(input byte) {
     hashResult += int(input)
-    hashResult *= 17
-    hashResult %= 256
+    hashResult *= MULTIPLY_H
+    hashResult %= DIVIDE_H
 }
 
+/* function to convert string type to integer */
 func toInt(numstr string) int {
     num, err := strconv.Atoi(numstr)
 
@@ -35,30 +43,42 @@ func toInt(numstr string) int {
     return num
 }
 
-func boxDetails(boxInput int, boxLabel string, lenseOperation, lenseFocal byte) {
-    if lenseOperation == byte('-') {
+/* function to gather boxdetails and proceed with further operations */
+func boxDetails(boxInput int, boxLabel string, lenseFocal byte) {
+    /* based on lense operation - or = 
+       deduction or appending or modifying will take place */
+    if byte(boxLabel[(len(boxLabel)-1)]) == byte('-') {
         for _, labels := range boxList[boxInput] {
-            if _, isLabelExists := labels[boxLabel[0:2]]; isLabelExists {
-                delete(labels, boxLabel[0:2])
+            if _, isLabelExists := labels[boxLabel[: (len(boxLabel)-1)]]; isLabelExists {
+                delete(labels, boxLabel[: (len(boxLabel)-1)])
             }
-        }
+        }   /* for deduction (-), if label is not present, nothing will happen */
     } else {
         for _, labels := range boxList[boxInput] {
-            if _, isLabelExists := labels[boxLabel[0:2]]; isLabelExists {
-                labels[boxLabel[0:2]] = toInt(string(lenseFocal))
+            if _, isLabelExists := labels[boxLabel[: (len(boxLabel)-1)]]; isLabelExists {
+                labels[boxLabel[: (len(boxLabel)-1)]] = toInt(string(lenseFocal))
                 return
             }
         }
-        boxList[boxInput] = append(boxList[boxInput], map[string]int{boxLabel[0:2] : toInt(string(lenseFocal))}) 
+
+        /* if already present, label's focal is changed (modified)
+           or label is appended to the box it belongs to */
+        boxList[boxInput] = append(boxList[boxInput], map[string]int{boxLabel[: (len(boxLabel)-1)] : toInt(string(lenseFocal))}) 
     }
+
 }
 
+/* function to find total focus power
+   focus power of a label is found by : ( not zero indexed )
+
+    (box) * (position in the given box) * (focal length)
+
+*/
 func totalFocus() {
     for list := range boxList {
         pos := 0
         for _, ele := range boxList[list] {
-            if len(ele) > 0{
-                fmt.Println(ele)
+            if len(ele) > 0{        /* empty maps not considered */
                 for _, focus := range ele {
                     focusResult += ((pos+1) * focus * (list+1))
                     pos++
@@ -86,18 +106,20 @@ func main() {
     /* buffer to read each character from input file */
     buf := bufio.NewReader(file)
 
+    /* variables to hold values needed for part two */
     lenseNext := false
-    lenseOperation := byte('-')
     boxInput := 0
     boxLabel := ""
+
     for {
         byteInput, err := buf.ReadByte() 
 
+        /* sends labels with lenseoperation and boxInput value from hash of labels */
         if lenseNext {
             if byteInput == byte(',') {
-                boxDetails(boxInput, boxLabel, lenseOperation, 0)
+                boxDetails(boxInput, boxLabel, 0)
             } else {
-                boxDetails(boxInput, boxLabel, lenseOperation, byteInput)
+                boxDetails(boxInput, boxLabel, byteInput)
             }
             lenseNext = false
         }
@@ -107,38 +129,30 @@ func main() {
         } else {
             /* discarding comma and newline characters */
             if byteInput == byte(',') || byteInput == byte('\n') {
-                sum+= hashResult
-                boxLabel = ""
+                sum+= hashResult        /* part one : after encountering comma */
+                boxLabel = ""           /* hash is added to sum */
                 hashResult = 0
                 continue
             } else {
-                if byteInput == byte('-') {
+                /* once lenseoperation is encountered hash value is stored 
+                   to find the box it belongs to */
+                if byteInput == byte('-') || byteInput == byte('=') {
                     boxInput = hashResult
-                    lenseOperation = byte('-')
-                    lenseNext = true
-                    
-                } else if byteInput == byte('=') {
-                    boxInput = hashResult
-                    lenseOperation = byte('=')
                     lenseNext = true
                 } 
+
                 findHash(byteInput)
-                boxLabel += string(byteInput)
+                boxLabel += string(byteInput) /* includes both label and lense operation */
             }
         }
     }
 
-    totalFocus()
+    totalFocus()    /* for part two */
 
     /* part one : result */
     fmt.Println("Part one (sum of hashes) : ", sum)
 
     /* part two : result */
     fmt.Println("Part two (sum of focal Lengths) : ", focusResult)
-
-    for _, ele := range boxList {
-        fmt.Println(ele)
-    }
-
 
 }
